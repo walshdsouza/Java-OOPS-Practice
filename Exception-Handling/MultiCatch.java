5a84048f37d54bf3bfee21622b4c13568c7e0cd3
Walsh D'Souza <walshdsouza10@gmail.com>
2026-07-16 23:18:07 +0530

    MultiCatch-Exception Handling

--- Exception-Handling/MultiCatch.java
+++ Exception-Handling/MultiCatch.java
@@ -4,6 +4,7 @@ public class MultiCatch{
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
         int n=sc.nextInt();
+        sc.nextLine();
         int[] arr = new int[n];
 
         System.out.println("Enter the elements of the array: ");
@@ -11,19 +12,22 @@ public class MultiCatch{
             System.out.print("Element "+(i+1)+": ");
             arr[i]=sc.nextInt();
         }
+        sc.nextLine();
         try{
             System.out.print("Enter the index to access(Starting from 0): ");
-            int index=sc.nextInt();
+            int index=Integer.parseInt(sc.nextLine());
             System.out.println("The element at index "+index+" is: "+arr[index]);
             System.out.print("Enter number to divide each array element by:");
-            int divisor=sc.nextInt();
+            int divisor=Integer.parseInt(sc.nextLine());
             for(int i=0; i<n; i++){
                 System.out.println("Element "+(i+1)+" after division: "+arr[i]/divisor);
             }
+        }catch (NumberFormatException e) {
+            System.err.println("Error: Please enter valid numeric characters only.");
         }catch(ArrayIndexOutOfBoundsException ae){
-            System.out.println("Error: Index is out of bounds.");
+            System.err.println("Error: Index is out of bounds.");
         }catch(ArithmeticException a){
-            System.out.println("Error: Division by zero is not allowed.");
+            System.err.println("Error: Division by zero is not allowed.");
         }
         sc.close();
     }
